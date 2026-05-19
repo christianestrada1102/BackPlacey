4af8ea4db64772941197724bacec4af6f6644bb3
christianestrada1102 <a1124150034@utch.edu.mx>
2026-05-18 18:17:01 -0600

    Fix backend for vercel deployment

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express()
 //aca le decimos middelware para json
 app.use(express.json())
+app.get('/', (req, res) => {
+    res.json({message: 'Placey Corrienod al 100'})
+})
 //configurar el puerto (Por convicion es el 3000)
 const usersRoutes = require('./routes/users')
 const placeysRoutes = require('./routes/placeys')
@@ -11,46 +14,31 @@ const catRoutes = require('./routes/cat')
 const cors = require('cors')
 const authRoutes = require('./routes/auth')
 
-const http = require('http')
-const { Server } = require('socket.io')
 const paymentRoutes = require('./routes/payment')
-const socketHandlander = require('./sockets/socketHandlander.js')
 
 
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+    ], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
 app.use('/users', usersRoutes)
 app.use('/placeys', placeysRoutes)
 app.use('/payments', paymentRoutes)
 app.use('/cat', catRoutes)
 app.use('/auth', authRoutes)
-app.use('/uploads', express.static('uploads'))
 
-const server = http.createServer(app)
-const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:5174',
-        methods: ['GET', 'POST']
-    }
-})
-
-        socketHandlander(io)
-
-
-const db = require ('./config/db')
 const PORT = process.env.PORT || 3014
 
-const testDBConnectionAndStart = async () => {
-    try {
-        await db.query('SELECT 1')
-        console.log('DB conectada pa')
-        server.listen(PORT, () => {
-            console.log(`Servidor corriendo en http://localhost:${PORT}`)
-        })
-    } catch (error) {
-        console.error('Error conectando la BD:', error.message)
-        process.exit(1)
-    }
+if (!process.env.VERCEL) {
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`)
+    })
 }
 
-testDBConnectionAndStart()
+module.exports = app
